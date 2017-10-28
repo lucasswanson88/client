62bfe3495faf1205bf85a3f002c669c44ff57175
Joe <forsmann@frontier.com>
2017-10-28 16:18:10 -0700

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    showAlert: boolean = false;
+
+    buttonClick() {
+        this.showAlert = !this.showAlert;
+    }
 }
